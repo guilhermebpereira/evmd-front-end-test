56f6016c03f53c1da8fb690d3fe262380fe9962f
ricardojunior02 <ricardojunior199502@gmail.com>
2020-03-27 12:39:26 -0300

    Adiciona useDispatch useSelector

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,50 +1,62 @@
+/* eslint-disable no-underscore-dangle */
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import {
   View, Text, StyleSheet, Image, TouchableOpacity,
 } from 'react-native';
 import PropTypes from 'prop-types';
 
-const UserCard = ({
-  name, email, age, picture, onPress,
-}) => (
-  <TouchableOpacity style={styles.container} onPress={onPress}>
-    <View>
-      <Image
-        source={{ uri: picture }}
-        style={styles.image}
-      />
-    </View>
-    <View>
-      <Text>{`${name}, ${age}`}</Text>
-      <Text>{email}</Text>
-    </View>
-  </TouchableOpacity>
-);
+
+function UserCard({ onPress, data }) {
+  const dispatch = useDispatch();
+
+
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => onPress(dispatch({ type: 'GET_USER', id: data._id }))}
+    >
+      <View>
+        <Image
+          source={{ uri: data.picture }}
+          style={styles.image}
+        />
+      </View>
+      <View style={styles.text}>
+        <Text>{`${data.name}, ${data.age}`}</Text>
+        <Text>{data.email}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
 
 UserCard.propTypes = {
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  age: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
+  data: PropTypes.oneOfType([PropTypes.object]).isRequired,
   onPress: PropTypes.func.isRequired,
 };
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-evenly',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#e5e5e5',
     padding: 15,
     borderRadius: 5,
     width: '100%',
     borderColor: '#e9e9e9',
+    marginTop: 5,
+  },
+  text: {
+    marginLeft: 15,
   },
   image: {
     borderRadius: 5,
     width: 50,
     height: 50,
   },
+
 });
 
 export default UserCard;
